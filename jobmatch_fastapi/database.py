56b8a83268e8914ce568bb3881544b75549fd519
i-ninte <slyobeng111@gmail.com>
2024-10-27 11:13:08 +0000

    db changed from mysql to postgresql

--- jobmatch_fastapi/database.py
+++ jobmatch_fastapi/database.py
@@ -10,9 +10,14 @@ load_dotenv()
 # MySQL database URL using mysqlclient
 SQLALCHEMY_DATABASE_URL = (
     
-    f"mysql+pymysql://{os.getenv('MYSQL_USER')}:{os.getenv('MYSQL_PASSWORD')}"
+   ''' f"mysql+pymysql://{os.getenv('MYSQL_USER')}:{os.getenv('MYSQL_PASSWORD')}"
     f"@{os.getenv('MYSQL_SERVER')}:{os.getenv('MYSQL_PORT')}/{os.getenv('MYSQL_DB')}"
-)
+
+    
+    '''
+    
+    "postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}"
+    f"@{os.getenv('POSTGRES_SERVER')}:{os.getenv('POSTGRES_PORT')}/{os.getenv('POSTGRES_DB')}")
 
 
 
